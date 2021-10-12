dc3a436fff21c749847b5c8c0acd5ee1b9c1a4bb
Ibrahim Abdellatif <55769032+ibrahimabdellatif@users.noreply.github.com>
2021-10-13 00:08:01 +0200

    Update DCMotorOn.java

--- app/src/main/java/com/example/controlview_iot/DCMotorOn.java
+++ app/src/main/java/com/example/controlview_iot/DCMotorOn.java
@@ -14,7 +14,7 @@ public class DCMotorOn extends AsyncTask<Void, Void, String> {
     @Override
     protected String doInBackground(Void... voids) {
         try {
-            URL url = new URL("http://192.168.1.33/motorOn");
+            URL url = new URL("http://192.168.1.33/dcON");
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             InputStream inputStream = httpURLConnection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
